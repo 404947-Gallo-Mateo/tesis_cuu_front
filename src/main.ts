4df8168df329ae28406541e58862a490d79848f6
404947_Gallo_Mateo <404947@tecnicatura.frc.utn.edu.ar>
2025-04-29 16:52:01 -0300

    se vuelve a agregar el provider de Routes necesarios para funcionamiento de <router-outlet>

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { KeycloakHelperService } from './app/services/keycloak-helper.service';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
     KeycloakHelperService,
     provideClientHydration()
   ]
